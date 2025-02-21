21822f4a16f8979b349c106f12f60f51e24faa66
Arjun19109 <bagriarjun2010@gmail.com>
2025-02-21 22:18:45 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,17 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int number = 12345; // Example number
+        int reversedNumber = reverseNumber(number);
+        System.out.println("Reversed Number: " + reversedNumber);
+    }
+
+    public static int reverseNumber(int num) {
+        int reversed = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        return reversed;
+    }
+}
